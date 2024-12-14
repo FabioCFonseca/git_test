2997832a900d4caf96faca0f2702b165a0677593
FabioCFonseca <fc_102@hotmail.com>
2024-12-14 11:20:43 -0300

    feat: rename TalkWithUsPage to TalkWithUsPage2

--- lib/modules/talk_with_us/talk_with_us_page.dart
+++ lib/modules/talk_with_us/talk_with_us_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class TalkWithUsPage extends StatelessWidget {
-  const TalkWithUsPage({super.key});
+class TalkWithUsPage2 extends StatelessWidget {
+  const TalkWithUsPage2({super.key});
 
   @override
   Widget build(BuildContext context) {
